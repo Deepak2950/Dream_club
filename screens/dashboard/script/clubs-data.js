142b68e48b0a9da833c918d6775554fefd67a682
Deepak <deepakboormane@gmail.com>
2022-05-10 02:27:30 +0530

    player details added in member dashboard

---
+++ screens/dashboard/script/clubs-data.js
@@ -0,0 +1,128 @@
+function getVacancy(sport, nVacancy) {
+  return { sport, nVacancy };
+}
+
+function getPlayer(name, age, role, pImage) {
+  return { name, age, role, pImage };
+}
+
+function getSports(name, players) {
+  return { name, players };
+}
+
+function getClubByClubName(clubName) {
+  return clubs.find((club) => {
+    return club.clubName == clubName;
+  });
+}
+
+function getClubJson(
+  clubName,
+  clubWebsite,
+  clubAddress,
+  clubImage,
+  clubDescription,
+  vacancy,
+  sports
+) {
+  return {
+    clubName,
+    clubWebsite,
+    clubAddress,
+    clubImage,
+    clubDescription,
+    vacancy,
+    sports,
+  };
+}
+
+let clubs = [
+  getClubJson(
+    "club1",
+    "website1",
+    "address of club1",
+    "./../../img/solid.png",
+    "this is good club1",
+    [
+      getVacancy("Football", 9),
+      getVacancy("Golf", 12),
+      getVacancy("Cricket", 3),
+    ],
+    [
+      getSports("Cricket", [
+        getPlayer("player1", "18", "ROle1", "./../../img/solid.png"),
+        getPlayer("player1", "18", "ROle2", "./../../img/solid.png"),
+        getPlayer("player1", "18", "ROle3", "./../../img/solid.png"),
+        getPlayer("player1", "18", "ROle4", "./../../img/solid.png"),
+        getPlayer("player1", "18", "ROle5", "./../../img/solid.png"),
+        getPlayer("player1", "118", "ROle6", "./../../img/solid.png"),
+      ]),
+      getSports("Football", [
+        getPlayer("player1ff", "18", "ROle1", "./../../img/solid.png"),
+        getPlayer("player1ff", "18", "ROle2", "./../../img/solid.png"),
+      ]),
+    ]
+  ),
+  getClubJson(
+    "club2",
+    "website2",
+    "address of club2",
+    "./../../img/solid.png",
+    "this is good club2",
+    [getVacancy("Football", 2), getVacancy("Cricket", 3)],
+    [
+      getSports("Cricket", [
+        getPlayer("player1ccc", "18", "ROle1", "./../../img/solid.png"),
+        getPlayer("player1ccc", "18", "ROle2", "./../../img/solid.png"),
+      ]),
+      getSports("Football", [
+        getPlayer("player1", "18", "ROle1", "./../../img/solid.png"),
+        getPlayer("player1", "18", "ROle2", "./../../img/solid.png"),
+      ]),
+    ]
+  ),
+  getClubJson(
+    "club3",
+    "website3",
+    "address of club3",
+    "./../../img/solid.png",
+    "this is good club3",
+    [
+      getVacancy("Football", 2),
+      getVacancy("Golf", 12),
+      getVacancy("Cricket", 3),
+    ],
+    [
+      getSports("Cricket", [
+        getPlayer("player1ccscww", "18", "ROle1", "./../../img/solid.png"),
+        getPlayer("player1cwcwcwc", "18", "ROle2", "./../../img/solid.png"),
+      ]),
+      getSports("Football", [
+        getPlayer("player1", "18", "ROle1", "./../../img/solid.png"),
+        getPlayer("player1", "18", "ROle2", "./../../img/solid.png"),
+      ]),
+    ]
+  ),
+  getClubJson(
+    "club4",
+    "website4",
+    "address of club4",
+    "./../../img/solid.png",
+    "this is good club4",
+    [
+      getVacancy("Football", 2),
+      getVacancy("Golf", 12),
+      getVacancy("Cricket", 3110),
+    ],
+    [
+      getSports("Cricket", [
+        getPlayer("player1", "18", "ROle1", "./../../img/solid.png"),
+        getPlayer("player1", "18", "ROle2", "./../../img/solid.png"),
+      ]),
+      getSports("Football", [
+        getPlayer("player1", "18", "ROle1", "./../../img/solid.png"),
+        getPlayer("player1", "18", "ROle2", "./../../img/solid.png"),
+      ]),
+    ]
+  ),
+];
--- screens/dashboard/script/member-dashboard-script.js
+++ screens/dashboard/script/member-dashboard-script.js
@@ -14,6 +14,8 @@ function removeActiveClassFromPlayerTabs() {
 
 function handlePlayerTabChange(tab) {
   removeActiveClassFromPlayerTabs();
+  currentSportsIndex = tab.dataset.index;
+  setClub();
 }
 
 function addOnClickToPlayerTabs() {
@@ -27,6 +29,101 @@ function addOnClickToPlayerTabs() {
   }
 }
 
-window.onload = function () {
+///// GLOBAL DATA
+let currentClubName = "club1";
+let currentClub;
+let currentSportsIndex = 0;
+
+function setCurrentSports(sportName) {
+  const club = getClubByClubName(currentClubName);
+  if (club !== undefined) {
+    getSportsByClub();
+  }
+}
+
+///// DATA
+function getClubPlayersAsHtml(players) {
+  let content = "";
+  for (let i = 0; i < players.length; i++) {
+    const player = players[i];
+    const { name, age, role, pImage } = player;
+
+    const cardContent = `
+    <div class="card">
+      <img
+        class="img-responsive rounded-circle"
+        src="${pImage}"
+      />
+      <h6>${name}</h6>
+      <p>Age : ${age}</p>
+      <p>Role : ${role}</p>
+    </div>
+    `;
+
+    content = content + cardContent;
+  }
+
+  return `
+  <div id="scroll" class="container">
+    <div class="row">
+      ${content}
+    </div>
+  </div>
+  `;
+}
+
+function getClubTagsAsHtml(sports) {
+  let content = "";
+  for (let i = 0; i < sports.length; i++) {
+    let liContent = `
+    <li class="nav-item" data-index="${i}" >
+      <a class="nav-link" aria-current="page" href="#">${sports[i].name}</a>
+    </li>
+    `;
+    content = content + liContent;
+  }
+
+  return `
+  <ul id="player-card-list" class="nav nav-tabs">
+    ${content}
+  </ul>
+  `;
+}
+
+function getClubCardAsHtml(club) {
+  const currentSports = club.sports[currentSportsIndex];
+
+  const content = `
+  <div class="card mt-5 mx-auto" style="max-width: 80%">
+      <div class="card-header">
+        <h3>${club.clubName}</h3>
+        <p class="mt-3">${club.clubDescription}</p>
+      </div>
+      <div class="card-body">
+        ${getClubTagsAsHtml(club.sports)}
+        <h5 class="card-title mt-4">Players: ${currentSports.name}</h5>
+        <!-- scroll  -->
+        ${getClubPlayersAsHtml(currentSports.players)}
+      </div>
+    </div>
+  `;
+  return content;
+}
+
+function setClub() {
+  const clubCard = document.getElementById("club-card");
+
+  const club = getClubByClubName(currentClubName);
+  currentClub = club;
+
+  const { clubName, clubAddress, clubImage, clubDescription, clubWebsite } =
+    club;
+
+  clubCard.innerHTML = getClubCardAsHtml(club);
+
   addOnClickToPlayerTabs();
+}
+
+window.onload = function () {
+  setClub();
 };
