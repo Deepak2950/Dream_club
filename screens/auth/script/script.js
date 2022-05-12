80421549970312e93e87cfe6d3f7852854dc814d
akash <akash.punagin@gmail.com>
2022-05-12 17:07:35 +0530

    connected all screens

--- screens/auth/script/script.js
+++ screens/auth/script/script.js
@@ -1,7 +1,7 @@
 function handleTabClick() {
   const registerAs = document.getElementById("register-as");
   let playerTab = document.getElementById("player-tab");
-  let memberTab = document.getElementById("member-tab");
+  let managerTab = document.getElementById("member-tab");
   console.log(playerTab.classList);
 
   if (playerTab.classList.contains("active")) {
@@ -13,12 +13,50 @@ function handleTabClick() {
     playerTab.classList.remove("text-dark");
   }
 
-  if (memberTab.classList.contains("active")) {
-    registerAs.innerHTML = "Register as Member";
-    memberTab.classList.add("text-dark");
-    memberTab.classList.remove("text-light");
+  if (managerTab.classList.contains("active")) {
+    registerAs.innerHTML = "Register as Manager";
+    managerTab.classList.add("text-dark");
+    managerTab.classList.remove("text-light");
   } else {
-    memberTab.classList.add("text-light");
-    memberTab.classList.remove("text-dark");
+    managerTab.classList.add("text-light");
+    managerTab.classList.remove("text-dark");
+  }
+}
+
+const isValidEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+function handleAuthLogin(form) {
+  const email = form.email.value;
+  const password = form.password.value;
+
+  if (!isValidEmail(email)) {
+    alert("Please enter valid email");
+  } else if (!password) {
+    alert("Please enter password");
+  } else if (email === "manager@gmail.com") {
+    window.location.pathname = "/screens/dashboard/manager-dashboard.html";
+  } else if (email === "player@gmail.com") {
+    window.location.pathname = "/screens/dashboard/player-dashboard.html";
+  } else {
+    alert("Email is not registered");
+  }
+}
+
+function handleAuthSignUp() {
+  console.log("Sign up");
+  let playerTab = document.getElementById("player-tab");
+  let managerTab = document.getElementById("member-tab");
+
+  if (playerTab.classList.contains("active")) {
+    window.location.pathname = "/screens/dashboard/player-dashboard.html";
+  }
+  if (managerTab.classList.contains("active")) {
+    window.location.pathname = "/screens/dashboard/manager-dashboard.html";
   }
 }
